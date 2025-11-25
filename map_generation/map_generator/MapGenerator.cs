6a50d970698e5ee769bfcf2d2031b5926e2105e9
Selkie Slatcher <selkieslatcher@gmail.com>
2025-11-24 20:48:05 -0500

    ...

--- map_generation/map_generator/MapGenerator.cs
+++ map_generation/map_generator/MapGenerator.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.NativeInterop;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -14,193 +15,207 @@ public partial class MapGenerator : Node2D
     GeometryUtils gu = new();
     RandomNumberGenerator rng = new();
 
-    int maxIterations = 10;
+    List<(Rect2, Godot.Color)> DebugRectDrawList = new();
+    List<Vector2[]> DebugPolyList = new();
+
+    const float targetTotalIslandWidth = 100;
+    float maxIslandWidth = 30;
+    float minIslandWidth = 5;    
+
+    public PhysicsDirectSpaceState2D Space { get; private set; }
+
 
     public override void _Ready()
     {
+        Space = PhysicsServer2D.SpaceGetDirectState(GetWorld2D().Space);
+
         GenerateTerrainMap();
-        
-        
         rng.Seed = (uint)TerrainPolySettings.HeightMapNoise.Seed;
+
+        QueueRedraw();
     }
-
-    public void SpawnIslands(List<LineSegment> spawnSourceLines, List<List<Vector2>> totalLand, int iterations = 0)
+    public override void _Draw()
     {
-        // steps of function ():
-        // b4 callings: turn the basic set of landmasses into spawn lines and passes that list and the landmasses
-        // add all lengths of spawnSourceLines together
+        foreach (var drawRect in DebugRectDrawList)
+        {
+            DrawRect(drawRect.Item1, drawRect.Item2, false, 1);
+        }
 
-       
-        // loop once to sum length (pass it in instead?) and find a random percentage of that length
-        var spawnLinesSum = 0f;
-        foreach (var line in spawnSourceLines) spawnLinesSum += line.End.X - line.Start.X;
-        var targetWidth = rng.Randf() * spawnLinesSum;
+        foreach (var poly in DebugPolyList)
+        {
+            // var colorArray = new Godot.Color[poly.Length];
+            // Array.Fill(colorArray, Godot.Colors.Black);
+            DrawPolyline(poly, Colors.Red);
+
+        }
+
         
-        // second loop to find which line, and at which point on that line, the randomized progress percentage sits
-        var targetPos = new Vector2();
-        var targetLine = new LineSegment(new Vector2(), new Vector2());
-        var currentProg = 0f;
-        foreach(var line in spawnSourceLines)
-        {
-            var widthRemaining = targetWidth - currentProg;
-            var lineStart = line.Start;
-            if (line.Start.X + widthRemaining < line.End.X)
-            {
-                targetPos = lineStart + new Vector2(widthRemaining, 0);
-                targetLine = line;
-                break;
-            }
-            currentProg += line.Length();
-            
-        }
-
-        // find a randomized point some distance from that point on the line
-        // validate that it can spawn at least a min sized island
-        var newSpawnCenter = new Vector2();
-
-        bool pointIsValid = false;
-        var loopLimit = 20;
-        var loops = 0;
-        var islandRect = new Rect2();
-        while (!pointIsValid && loops < loopLimit)
-        {
-            loops++;
-            var maxDist = 50f;
-            var randDist = (float)Math.Pow(rng.Randf(),2) * maxDist;
-            var randAngle = float.Pi * 2 * rng.Randf();
-            var randPos = (Vector2.Right * randDist).Rotated(randAngle) + targetPos;
-
-            var maxRectSize = MaxNonIntersectingRectSize(randPos, new Vector2(10,10), new Vector2(40, 30), totalLand);
-            
-            if (maxRectSize == Vector2.Zero) continue;
-            else
-            {
-                pointIsValid = true;
-                newSpawnCenter = randPos;
-                islandRect = gu.RectFromCenterPoint(newSpawnCenter, maxRectSize);
-
-            }
-        }
-
-        if (pointIsValid)
-        {
-            var newLandPoly = gu.PolygonFromRect(islandRect);
-            var spawnLineEnd = islandRect.End;
-            var spawnLineStart = new Vector2(islandRect.Position.X, islandRect.End.Y);
-            spawnSourceLines.Add(new LineSegment(spawnLineEnd, spawnLineStart));
-            totalLand.Add(newLandPoly.ToList());
-
-        }
-
-        // find max allowed rect size for that spot
-
     }
-
-    public Vector2 MaxNonIntersectingRectSize(Vector2 centerPoint, Vector2 minSize, Vector2 maxSize, List<List<Vector2>> totalLandmasses )
+    public void AddRectToDrawList(Rect2 rect, Godot.Color? color = null)
     {
-
-        var sizeSteps = 5;
-        var maximizedSize = Vector2.Zero;
-        for (int i = 0; i < sizeSteps; i++)
-        {
-            var rectSize = ( (i+1) / sizeSteps ) * (maxSize-minSize) + minSize;
-            var rect = gu.RectFromCenterPoint(centerPoint, rectSize);
-            bool intersects = RectIntersectsLandmasses(totalLandmasses, rect);
-            if (intersects) break;
-            else maximizedSize = rectSize;
-        }
-        return maximizedSize;
-    }
-
-    public bool RectIntersectsLandmasses(List<List<Vector2>> landmasses, Rect2 rect)
+        if (color == null) color = Colors.Black;
+        DebugRectDrawList.Add((rect, (Godot.Color)color));
+    } 
+    
+    private void AddPolyToDrawList(Vector2[] polygon)
     {
-        var rectPoints = gu.PolygonFromRect(rect);
-        foreach(var land in landmasses)
-        {
-            foreach(var p in rectPoints)
-            {   
-                if (Geometry2D.IsPointInPolygon(p, land.ToArray()))
-                {
-                    return true;
-                }
+        DebugPolyList.Add(polygon);
+    }
 
-            }
-        }
+    
 
-        return false;
+    // notes on new spawn islands function
+    // 1. create dedicated "spawn zone" function that takes a rect, grabs its top line, and creates another rect around it 
+    //
+    // 2. then pass to a funtion to sample randomly inside that rect
+    //
+    // 3. pass along spawn zones, (for now) just pick a random spawn zone above a specific width
+    // 4. track total landmass area and stop after target
+    // 5. 
 
+    public Rect2 GetSpawnZoneFromTerrainRect(Rect2 terrain)
+    {
+        var spawnRect = new Rect2(terrain.Position + new Vector2(0, terrain.Size.Y), new Vector2(terrain.Size.X, 0));
+        spawnRect = spawnRect.GrowIndividual(15, 15, 15, 15);
+       // eventually add clamping to a min and max range to top and side spawn distaces
+        return spawnRect;
 
     }
 
+    public Vector2 GetRandomPointInSpawnRect(Rect2 SpawnRect)
+    {
+        var size = SpawnRect.Size;
+        var pos = SpawnRect.Position;
+        var x = rng.Randf() * size.X + pos.X;
+        var y = rng.Randf() * size.Y + pos.Y;
+        return new Vector2(x,y);
+    }
+   
     public void GenerateTerrainMap()
     {
         SetHeightMap();
 
-        // pre-lim steps:
-        // 1. get a full set of height map rects for the entire width
-        // 2. split into islands (minor breaks at low points and major breaks at division levels ) 
-        // 3. convert each group into a simplified map shape (cliff edges and cave insets)
-        // 4. use simply polygons to find placements for complex islands (random sampling )
-        
-        var edgeNoise = new NoiseEdgePolygon();
+        // var edgeNoise = new NoiseEdgePolygon();
         var terrainPolygon = new TerrainPolygonMap();
         terrainPolygon.Settings = TerrainPolySettings;
-        edgeNoise.Settings = TerrainPolySettings; 
+        // edgeNoise.Settings = TerrainPolySettings; 
 
         // get platforms and separate them at major division points (will be realigned later using rects)
         var mapRange = new Vector2(0, MapSettings.Width);
         var rects = heightMap.GetRects(mapRange);
         var landGroups = GroupHeightMapRects(rects);
+        var landPolygons = landGroups.Select(l => terrainPolygon.GetSimpleTerrainPoly(l).ToArray()).ToList();
+        var landPolyRects = new List<Rect2>();
         
-
-        var polygonGroups = landGroups.Select(l => terrainPolygon.GetSimpleTerrainPoly(l).ToList()).ToList();
-        // for
-
-        
-        List<LineSegment> initSpawnLines = new();
-        foreach (var poly in polygonGroups)
+        // translate polygons and create water gaps
+        var lastEndPos = Vector2.Zero;
+        for (int i = 0; i < landPolygons.Count; i++)
         {
-            var rect = gu.RectFromPolygon(poly.ToArray());
-            var spawnLineEnd = rect.End;
-            var spawnLineStart = new Vector2(rect.Position.X, rect.End.Y);
-            initSpawnLines.Add(new LineSegment(spawnLineEnd, spawnLineStart));
+            var land = landPolygons[i];
+            var rect = gu.RectFromPolygon(land);
+            var translation = lastEndPos - rect.Position;
+            landPolygons[i] = gu.TranslatePolygon(land, translation);
+            rect.Position = lastEndPos;
+            landPolyRects.Add(rect);
+            
+            // add polygon to collision
+            var colShape = PhysicsShapeFromPolygon(landPolygons[i]);
+            AddAreaToSpace(colShape, Vector2.Zero);
+            AddPolyToDrawList(landPolygons[i]);
+
+            var waterGap = 30f;
+            lastEndPos = rect.Position + new Vector2(rect.Size.X + waterGap, 0);
+
+            // AddRectToDrawList(rect);
         }
-        
-        GD.Print("pre island count: ", polygonGroups.Count);
-        SpawnIslands(initSpawnLines, polygonGroups);
-        GD.Print("post island count: ", polygonGroups.Count);
-
-
-        // var simplePolyRects = polygonGroups.Select(gu.RectFromPolygon);
-        // var landGroupsNoisy = polygonGroups.Select(edgeNoise.ApplyNoiseEdge);
-        
+        var spawnRects = landPolyRects.Select(GetSpawnZoneFromTerrainRect).ToList();
 
         
 
-
-
-        // var startX = 0f;
-        // for (int i = 0; i < landGroupsNoisy.Count(); i++)
-        // {
-        //     var land = landGroupsNoisy.ElementAt(i);
-        //     var rect = simplePolyRects.ElementAt(i);
-        //     var poly2d = new Polygon2D(){Polygon = land};
-        //     poly2d.Position = new Vector2(startX, 0);
-        //     var line = gu.Line2DFromPolygon(land, 0.5f, Colors.Red);
-        //     line.Position = poly2d.Position;            
-        //     startX += rect.Size.X + 30;
-
-        //     AddChild(line);
-        //     AddChild(poly2d);
-        // }
-
-        
-        
-
+        SpawnIslands(landPolygons, spawnRects, 0);
         
     }
 
-  
+   
+
+
+    private void SpawnIslands(List<Vector2[]> lands, List<Rect2> spawningRects, float accumLandWidth, float targetLandWidth = targetTotalIslandWidth)
+    {
+        // pick random spawn rect
+        var spawnRect = spawningRects[rng.RandiRange(0, spawningRects.Count-1)];
+
+        // get valid point:
+        // after a non-intersecting point is chosen, binary search looks for max size rect to fit in area
+        // if area too tight, loop repeats and new point chosen
+        // if this outer loop fails X num of times (shoud not often fail) no island is chosen and recursion ends
+        Rect2 islandRect = new();
+        Vector2 spawnPoint;
+        int rectLoops = 0;
+        do
+        {
+            spawnPoint = GetRandomPointInSpawnRect(spawnRect);
+            if (IsPointColliding(spawnPoint)) 
+            {
+                GD.Print("point intersecting");
+                continue;
+            }  // initial check for point collision before building rect;
+            
+            var aspectRatio = rng.RandfRange(0.5f,1); //randomly range from square to double wide
+            var minSize = new Vector2(minIslandWidth, minIslandWidth * aspectRatio);
+            var maxSize = new Vector2(maxIslandWidth, maxIslandWidth * aspectRatio);
+            
+            Rid rectShape;
+            var steps = 0; var stepLimit = 12; 
+            while(steps++ < stepLimit)
+            {
+                var midSize = minSize + (maxSize - minSize) / 2;
+            
+                rectShape = PhysicsShapeFromRectangle(midSize);
+                bool isCollide = IsShapeColliding(rectShape, spawnPoint);
+
+                if (isCollide) maxSize = midSize;
+                else minSize = midSize;
+
+            }
+
+            // if minsize is too close to its original value after step limit reached, assume no valid rect found
+            // else treat min size as near maximum possible rect size
+            if (minSize.X - minIslandWidth < 2) continue; // start over with new spawn point
+            else
+            {
+                islandRect = gu.RectFromCenterPoint(spawnPoint, minSize);
+                break;
+            }
+
+
+        } while (rectLoops++ < 100);
+        
+        // no valid rect found, end recursion
+        if  (islandRect.Size == Vector2.Zero) return;
+
+        // convert island rect into an island poly
+        // add the land and the spawning rect to lists, and add to accumulated width
+        // recurse with updated list if not enough width accumlated
+        var islandPoly = gu.PolygonFromRect(islandRect);
+        lands.Add(islandPoly);
+        spawningRects.Add(GetSpawnZoneFromTerrainRect(islandRect));
+        AddRectToDrawList(islandRect);
+
+        
+
+        var shape = PhysicsShapeFromRectangle(islandRect.Size);
+        AddAreaToSpace(shape, spawnPoint);
+
+        accumLandWidth += islandRect.Size.X;
+        GD.Print(accumLandWidth);
+        if (accumLandWidth > targetLandWidth) return;
+
+        SpawnIslands(lands, spawningRects, accumLandWidth, targetLandWidth);
+    }
+
+
+
+
     
     private List<List<Rect2>> GroupHeightMapRects(List<Rect2> rects)
     {
@@ -237,5 +252,67 @@ public partial class MapGenerator : Node2D
         heightMap.Height = TerrainPolySettings.MaxHeight;
         heightMap.Jaggedness = TerrainPolySettings.Jaggedness;
     }
+    public Rid PhysicsShapeFromPolygon(Vector2[] polygon)
+    {
+        var shape = PhysicsServer2D.ConcavePolygonShapeCreate();
+        
+        // convert polygon into format for ShapeSetData (in sets of 2 forming segments)
+        List<Vector2> pointSegmentList = new();
+        var segments = gu.LineSegmentsFromPolygon(polygon);
+        foreach (var seg in segments)
+        {
+            pointSegmentList.AddRange([seg.Start, seg.End]);
+        }
+        
+        PhysicsServer2D.ShapeSetData(shape, pointSegmentList.ToArray());
+        return shape;
+    }
+    public Rid PhysicsShapeFromRectangle(Vector2 size)
+    {
+        var shape = PhysicsServer2D.RectangleShapeCreate();
+        PhysicsServer2D.ShapeSetData(shape, size/2);
+        return shape;
+    }
+    public void AddAreaToSpace(Rid shape, Vector2 origin)
+    {
+        var area = PhysicsServer2D.AreaCreate();
+        var transform = Transform2D.Identity;
+        transform.Origin = origin;
+        PhysicsServer2D.AreaAddShape(area, shape);
+        
+        PhysicsServer2D.AreaSetTransform(area, transform);
+
+		PhysicsServer2D.AreaSetSpace(area, GetWorld2D().Space);
+        
+    }
+    public void RemoveAreaFromSpace(Rid area)
+    {
+        PhysicsServer2D.AreaSetSpace(area, new Rid());
+    }
+    public bool IsShapeColliding(Rid shape, Vector2 shapeOrigin)
+    {
+        var trans = Transform2D.Identity;
+        trans.Origin = shapeOrigin;
+        var query = new PhysicsShapeQueryParameters2D()
+        {
+            ShapeRid = shape,
+            Transform = trans,
+            CollideWithAreas = true,
+
+        };
+        return Space.GetRestInfo(query).Count != 0;
+
+    }
+    public bool IsPointColliding(Vector2 point)
+    {
+        var query = new PhysicsPointQueryParameters2D()
+        {
+            Position = point,
+            CollideWithAreas = true
+        };
+        return Space.IntersectPoint(query, 1).Count != 0;
+    }
+
+  
 
 }
